d7adfcb2d7ca9640c2cb4be2e228eecb6402286e
vbrenister <v.brenister@newmotion.com>
2021-12-14 07:59:36 +0100

    Optimized spacer

--- src/features/components/spacer/spacer.component.js
+++ src/features/components/spacer/spacer.component.js
@@ -1,36 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
 
-const TopSmall = styled.View`
-  margin-top: ${(props) => props.theme.space[1]};
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
+
+const positionVariant = {
+  top: "margin-top",
+  left: "margin-left",
+  bottom: "margin-bottom",
+  right: "margin-right",
+};
+
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const selectedPosition = positionVariant[position];
+  return `${selectedPosition}:${theme.space[sizeIndex]}`;
+};
+
+export const Spacer = styled.View`
+  ${({ position, size, theme }) => getVariant(position, size, theme)}
 `;
 
-const TopMedium = styled.View`
-  margin-top: ${(props) => props.theme.space[2]};
-`;
-
-const TopLarge = styled.View`
-  margin-top: ${(props) => props.theme.space[3]};
-`;
-
-const LeftSmall = styled.View`
-  margin-left: ${(props) => props.theme.space[1]};
-`;
-
-const LeftMedium = styled.View`
-  margin-left: ${(props) => props.theme.space[2]};
-`;
-
-const LeftLarge = styled.View`
-  margin-left: ${(props) => props.theme.space[3]};
-`;
-
-export const Spacer = ({ variant }) => {
-  if (variant === "top.medium") return <TopMedium />;
-  if (variant === "top.large") return <TopLarge />;
-  if (variant === "left.small") return <LeftSmall />;
-  if (variant === "left.medium") return <LeftMedium />;
-  if (variant === "left.large") return <LeftLarge />;
-
-  return <TopSmall />;
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
 };
